3c4ea3f978a5a5c81af7ff9d423cfa6d4d4bbda7
yaksh <panchalyaksh512@gmail.com>
2023-01-13 01:02:43 +0530

    crating login page_0.2..date_13_jan

--- Dev_yaksh/lib/main.dart
+++ Dev_yaksh/lib/main.dart
@@ -1,97 +1,76 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  static const String _title = 'Log In';
-
+void main() => runApp(MaterialApp(
+  home : Home(),
+));
+class Home extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: _title,
-      home: Scaffold(
-        appBar: AppBar(title: const Text(_title)),
-        body: const MyStatefulWidget(),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Log In'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[800],
+        elevation: 0.0,
       ),
+      body:Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+        
+        child: Column(
+          children: <Widget>[
+            Container(                  // row-1
+              child: Row(
+                children: <Widget>[
+
+                  Text(
+                    'Your Name :',
+                    style: TextStyle(
+                      color: Colors.cyan,
+                      letterSpacing: 2,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            Container(// row-2
+              child: Divider(
+                height: 30,
+                color: Colors.red,
+
+              ),
+            ),
+
+            Container(                  // row-1
+              child: Row(
+                children: <Widget>[
+
+                  Text(
+                    'Your Email :',
+                    style: TextStyle(
+                      color: Colors.cyan,
+                      letterSpacing: 2,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            Container(// row-2
+              child: Divider(
+                height: 30,
+                color: Colors.red,
+
+              ),
+            )
+          ],
+        ),
+      )
+
+
+        
+      
+
     );
   }
 }
-
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
-
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  TextEditingController nameController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.all(10),
-        child: ListView(
-          children: <Widget>[
-            Container(
-                alignment: Alignment.topLeft,
-                padding: const EdgeInsets.all(10),
-                child: const Text(
-                  'Welcome',
-                  style: TextStyle(
-                      color: Colors.blue,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 30),
-                )),
-            Container(
-                alignment: Alignment.topLeft,
-                padding: const EdgeInsets.all(10),
-                child: const Text(
-                  'Sign in',
-                  style: TextStyle(fontSize: 20),
-                )),
-            Container(
-
-              padding: const EdgeInsets.fromLTRB(10,5,5,0),
-              child: TextField(
-
-                controller: nameController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'user name',
-                ),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(10, 5, 5, 0),
-              child: TextField(
-                obscureText: true,
-                controller: passwordController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Email',
-                ),
-              ),
-            ),
-
-
-
-            Container(
-                height: 50,
-                padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
-                child: ElevatedButton(
-                  child: const Text('Login'),
-                  onPressed: () {
-                    print(nameController.text);
-                    print(passwordController.text);
-                  },
-                )
-            ),
-
-          ],
-        ));
-  }
-}
-
